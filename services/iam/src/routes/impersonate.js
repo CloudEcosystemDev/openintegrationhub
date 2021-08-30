c3dd0d9b37ac280730791b9080249af5f18322ce
Jorge Russo <jorge.russo@cloudecosystem.org>
2021-08-30 14:36:44 +0200

    Added logs to impersonate endpoint

--- services/iam/src/routes/impersonate.js
+++ services/iam/src/routes/impersonate.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const Logger = require('@basaas/node-logger');
 const authMiddleware = require('../util/auth');
 const CONSTANTS = require('./../constants/index');
 const AccountDAO = require('../dao/accounts');
 const Account = require('../models/account');
-
-const { ObjectId } = mongoose.Types;
+const conf = require('../conf');
 
 const TokenUtils = require('./../util/tokens');
+
+const log = Logger.getLogger(`${conf.general.loggingNameSpace}/app`, {
+    level: 'debug',
+});
 
 const router = express.Router();
 
@@ -25,6 +28,7 @@ const endUserPermissions = [
 ];
 
 const impersonateLogin = async (req, res, next) => { 
+    log.info('In impersonate middleware...');
     if (!req.user.tenant) {
         return next({ status: 401, message: 'You have to belong to a tenant' });
     }
@@ -61,7 +65,7 @@ const impersonateLogin = async (req, res, next) => {
 };
 
 router.post('/', impersonateLogin, async (req, res, next) => {
-
+    log.info('In impersonate endpoint...');
     if (!req.user) {
         return next({ status: 401, message: CONSTANTS.ERROR_CODES.NOT_LOGGED_IN });
     }
