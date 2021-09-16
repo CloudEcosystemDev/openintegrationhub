7d15143028023788f2dcce94062dc22117bed180
Jorge Russo <jorge.russo@cloudecosystem.org>
2021-09-16 16:55:53 +0200

    Moved ssevents endpoint after iam middleware

--- services/secret-service/src/server/index.js
+++ services/secret-service/src/server/index.js
@@ -53,8 +53,9 @@ module.exports = class Server {
         apiBase.use('/secrets', cors(this.corsOptions));
 
         apiBase.use('/callback', require('../route/callback'));
-        apiBase.use('/ssevent', require('../route/sse'));
         apiBase.use(this.iam.middleware);
+        apiBase.use('/ssevent', require('../route/sse'));
+
 
         // setup routes
 
