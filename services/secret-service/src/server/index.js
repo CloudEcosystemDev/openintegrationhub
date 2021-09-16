8d0948e15eae5942dc47257a8defcfd46caa40ff
Jorge Russo <jorge.russo@cloudecosystem.org>
2021-09-16 16:46:27 +0200

    Removed cors in ssevent endpoint

--- services/secret-service/src/server/index.js
+++ services/secret-service/src/server/index.js
@@ -51,7 +51,6 @@ module.exports = class Server {
         apiBase.use('/callback', cors(this.corsOptions));
         apiBase.use('/auth-clients', cors(this.corsOptions));
         apiBase.use('/secrets', cors(this.corsOptions));
-        apiBase.use('/ssevent', cors(this.corsOptions));
 
         apiBase.use('/callback', require('../route/callback'));
         apiBase.use('/ssevent', require('../route/sse'));
