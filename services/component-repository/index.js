b8292ed16678a65c0d49ef6c974769e563e96157
Pavel Nedelko <pnedelko@gmail.com>
2023-08-16 00:11:04 +0200

    add uncaughtException and unhandledRejection handlers

--- services/component-repository/index.js
+++ services/component-repository/index.js
@@ -2,6 +2,12 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '.env') });
 const App = require('./src/App.js');
 
+process.on('uncaughtException', (err, origin) => console.error(err, origin));
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    // Application specific logging, throwing an error, or other logic here
+});
+
 (async () => {
     try {
         const app = new App();
